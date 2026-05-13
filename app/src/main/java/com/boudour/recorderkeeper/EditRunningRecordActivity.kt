c26632eeaa933790d52351a50bfd7d7b87be13be
boudour ayari <ayariboudour@gmail.com>
2026-05-13 15:59:47 +0200

    Dynamic ToolBar add

---
+++ app/src/main/java/com/boudour/recorderkeeper/EditRunningRecordActivity.kt
@@ -0,0 +1,20 @@
+package com.boudour.recorderkeeper
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.boudour.recorderkeeper.databinding.ActivityEditRunningRecordBinding
+
+class EditRunningRecordActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityEditRunningRecordBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityEditRunningRecordBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val distance = intent.getStringExtra("Distance")
+        title = "$distance Record"
+    }
+}
--- app/src/main/java/com/boudour/recorderkeeper/RunningFragement.kt
+++ app/src/main/java/com/boudour/recorderkeeper/RunningFragement.kt
@@ -1,17 +1,49 @@
 package com.boudour.recorderkeeper
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.boudour.recorderkeeper.databinding.FragementRunningBinding
 
 class RunningFragement : Fragment() {
+
+    private lateinit var binding: FragementRunningBinding
+
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        return inflater.inflate(R.layout.fragement_running, container, false)
+    ): View {
+        binding = FragementRunningBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        setupClickListeners()
+    }
+
+    private fun setupClickListeners(){
+        binding.container5km.setOnClickListener {
+            launchRunningRecordScreen("5km")
+        }
+        binding.container10km.setOnClickListener {
+            launchRunningRecordScreen("10km")
+        }
+        binding.containerHalfMarathon.setOnClickListener {
+            launchRunningRecordScreen("Half Marathon")
+        }
+        binding.containerMarathon.setOnClickListener {
+            launchRunningRecordScreen("Marathon")
+        }
+    }
+
+    private fun launchRunningRecordScreen(distance: String) {
+       val intent = Intent(context, EditRunningRecordActivity::class.java)
+        intent.putExtra("Distance", distance)
+        startActivity(intent)
     }
 }
