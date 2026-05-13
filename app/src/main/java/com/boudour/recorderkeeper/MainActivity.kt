a55cb2bea26fcccf1a06bb0b6e10033cb8feb9dc
boudour ayari <ayariboudour@gmail.com>
2026-05-13 13:03:26 +0200

    Add our first Fragements

--- app/src/main/java/com/boudour/recorderkeeper/MainActivity.kt
+++ app/src/main/java/com/boudour/recorderkeeper/MainActivity.kt
@@ -5,6 +5,7 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.fragment.app.commit
 import com.boudour.recorderkeeper.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -13,8 +14,14 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        // add fragement transaction
+        supportFragmentManager.commit {
+            add(R.id.frame_contentt, RunningFragement())
+
+        }
     }
 }
