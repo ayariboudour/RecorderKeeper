e820888f273c0097fb670b15f8361cee58ccc18a
boudour ayari <ayariboudour@gmail.com>
2026-05-26 13:16:57 +0200

    Add cycling fragement

--- app/src/main/java/com/boudour/recorderkeeper/CyclingFragement.kt
+++ app/src/main/java/com/boudour/recorderkeeper/CyclingFragement.kt
@@ -1,19 +1,40 @@
 package com.boudour.recorderkeeper
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.boudour.recorderkeeper.databinding.FragementCyclingBinding
 
 class CyclingFragement : Fragment() {
+
+    private lateinit var binding: FragementCyclingBinding
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        return inflater.inflate(R.layout.fragement_cycling, container, false)
+    ): View {
+        binding = FragementCyclingBinding.inflate(inflater, container, false)
+        return binding.root
+    }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        setupClickListeners()
+    }
+
+    private fun setupClickListeners() {
+        binding.containerLongestRide.setOnClickListener { launchCyclingRecordScreen("Longest Ride") }
+        binding.containerBiggestClimb.setOnClickListener { launchCyclingRecordScreen("Biggest Climb") }
+        binding.containerBestAverageSpeed.setOnClickListener { launchCyclingRecordScreen("Best Average Speed") }
+    }
+
+    private fun launchCyclingRecordScreen(distance: String) {
+        val intent = Intent(context, EditCyclingRecordActivity::class.java)
+        intent.putExtra("Distance", distance)
+        startActivity(intent)
     }
 
 }
---
+++ app/src/main/java/com/boudour/recorderkeeper/EditCyclingRecordActivity.kt
@@ -0,0 +1,17 @@
+package com.boudour.recorderkeeper
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.boudour.recorderkeeper.databinding.ActivityEditCyclingRecordBinding
+
+class EditCyclingRecordActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityEditCyclingRecordBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityEditCyclingRecordBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val distance = intent.getStringExtra("Distance")
+        title = "$distance Record"
+    }
+}
