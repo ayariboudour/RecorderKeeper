53d9c0f6ecde6ac8b642e427161d4029d4a2368c
boudour ayari <ayariboudour@gmail.com>
2026-06-02 13:37:56 +0200

    add display data on the cycling fragement

--- app/src/main/java/com/boudour/recorderkeeper/cycling/CyclingFragement.kt
+++ app/src/main/java/com/boudour/recorderkeeper/cycling/CyclingFragement.kt
@@ -1,5 +1,6 @@
 package com.boudour.recorderkeeper.cycling
 
+import android.content.Context
 import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
@@ -10,8 +11,8 @@ import com.boudour.recorderkeeper.databinding.FragementCyclingBinding
 import com.boudour.recorderkeeper.editRecord.EditRecordActivity
 
 class CyclingFragement : Fragment() {
-
     private lateinit var binding: FragementCyclingBinding
+
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
@@ -26,15 +27,56 @@ class CyclingFragement : Fragment() {
         setupClickListeners()
     }
 
+    override fun onResume() {
+        super.onResume()
+        displayCyclingRecords()
+    }
+
+    private fun displayCyclingRecords() {
+        val cyclingPreferences =
+            requireContext().getSharedPreferences("cycling", Context.MODE_PRIVATE)
+
+        binding.textViewLongestRideValue.text =
+            cyclingPreferences.getString("Longest Ride record", null)
+        binding.textViewLongestRideDate.text =
+            cyclingPreferences.getString("Longest Ride date", null)
+        binding.textViewBiggestClimbValue.text =
+            cyclingPreferences.getString("Biggest Climb record", null)
+        binding.textViewBiggestClimbDate.text =
+            cyclingPreferences.getString("Biggest Climb date", null)
+        binding.textViewBestAverageSpeedValue.text =
+            cyclingPreferences.getString("Best Average Speed record", null)
+        binding.textViewBestAverageSpeedDate.text =
+            cyclingPreferences.getString("Best Average Speed date", null)
+    }
+
     private fun setupClickListeners() {
-        binding.containerLongestRide.setOnClickListener { launchCyclingRecordScreen("Longest Ride", "Distance") }
-        binding.containerBiggestClimb.setOnClickListener { launchCyclingRecordScreen("Biggest Climb", "Height") }
-        binding.containerBestAverageSpeed.setOnClickListener { launchCyclingRecordScreen("Best Average Speed", "Average Speed") }
+        binding.containerLongestRide.setOnClickListener {
+            launchCyclingRecordScreen(
+                "Longest Ride",
+                "Distance"
+            )
+        }
+        binding.containerBiggestClimb.setOnClickListener {
+            launchCyclingRecordScreen(
+                "Biggest Climb",
+                "Height"
+            )
+        }
+        binding.containerBestAverageSpeed.setOnClickListener {
+            launchCyclingRecordScreen(
+                "Best Average Speed",
+                "Average Speed"
+            )
+        }
     }
 
     private fun launchCyclingRecordScreen(record: String, recordFieldHint: String) {
         val intent = Intent(context, EditRecordActivity::class.java)
-        intent.putExtra("screen_data", EditRecordActivity.ScreenData(record, "cycling", recordFieldHint))
+        intent.putExtra(
+            "screen_data",
+            EditRecordActivity.ScreenData(record, "cycling", recordFieldHint)
+        )
         startActivity(intent)
     }
 
--- app/src/main/java/com/boudour/recorderkeeper/running/RunningFragement.kt
+++ app/src/main/java/com/boudour/recorderkeeper/running/RunningFragement.kt
@@ -26,7 +26,6 @@ class RunningFragement : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         setupClickListeners()
-        displayRunningRecords()
     }
 
     override fun onResume() {
