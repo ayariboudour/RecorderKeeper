35a2caeb334a4bea749f1cc2a80889a4034c3379
boudour ayari <ayariboudour@gmail.com>
2026-06-02 12:36:35 +0200

    improve the edit record

--- app/src/main/java/com/boudour/recorderkeeper/running/EditRunningRecordActivity.kt
+++ app/src/main/java/com/boudour/recorderkeeper/running/EditRunningRecordActivity.kt
@@ -1,45 +1,57 @@
 package com.boudour.recorderkeeper.running
 
 import android.content.Context
+import android.content.SharedPreferences
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.content.edit
 import com.boudour.recorderkeeper.databinding.ActivityEditRunningRecordBinding
 
 class EditRunningRecordActivity : AppCompatActivity() {
+
     private lateinit var binding: ActivityEditRunningRecordBinding
+    private val runningPreference by lazy { getSharedPreferences("running", Context.MODE_PRIVATE) }
+    private val distance by lazy { intent.getStringExtra("Distance") }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityEditRunningRecordBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        setupUi()
+        displayRecord()
+    }
 
-        val distance = intent.getStringExtra("Distance")
+    private fun setupUi() {
         title = "$distance Record"
-
-        // 1. Load the record when the activity starts
-        displayRecord(distance)
-
         binding.buttonSave.setOnClickListener {
-            saveRecord(distance)
+            saveRecord()
             finish() // Go back to the previous screen after saving
+        }
+        binding.buttonDelete.setOnClickListener {
+            clearRecord()
+            finish()
         }
     }
 
-    private fun displayRecord(distance: String?) {
-        val runningPreference = getSharedPreferences("running", Context.MODE_PRIVATE)
+    private fun displayRecord() {
         binding.editTextRecord.setText(runningPreference.getString("$distance record", null))
         binding.editTextDate.setText(runningPreference.getString("$distance date", null))
     }
 
-    private fun saveRecord(distance: String?) {
+    private fun saveRecord() {
         val record = binding.editTextRecord.text.toString()
         val date = binding.editTextDate.text.toString()
 
-        val runningPreference = getSharedPreferences("running", Context.MODE_PRIVATE)
         runningPreference.edit {
             putString("$distance record", record)
             putString("$distance date", date)
+        }
+    }
+
+    private fun clearRecord() {
+        runningPreference.edit {
+            remove("$distance record")
+            remove("$distance date")
         }
     }
 }
